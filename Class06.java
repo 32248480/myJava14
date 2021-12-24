6ffbc03086206377b5b7bb391f65cb2f262557b7
32248480 <92283556+32248480@users.noreply.github.com>
2021-12-24 13:19:26 +0800

    Create Class06.java

---
+++ Class06.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class Main{
+public static void main(String args[]){
+try{
+ char data[]=new char[128];
+ FileReader fr=new FileReader("C:\\Users\\user\\eclipse");
+
+ int num=fr.read(data);
+ String str=new String(data,0,num);
+ System.out.println("Characters read= "+num);
+ System.out.println(str);
+ fr.close();
+}
+catch(IOException e){
+}	
+}	
+}
+
+//Characters read= 29
+//火車快飛,火車快飛
+//越過高山,飛過小溪
+//不知走了幾百里
